590667c78ff1097910d179c55173a4252ef0b29d
dariarosca <daria.rosca@yahoo.com>
2022-11-09 14:54:09 +0200

    Add files via upload

---
+++ DestryTheWorld/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  DestryTheWorld
+//
+//  Created by Rosca Daria on 09.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+       print ("Hello world!!!!")
+    
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DestryTheWorld/DestryTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestryTheWorldApp.swift
+//  DestryTheWorld
+//
+//  Created by Rosca Daria on 09.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct DestryTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
